4b8c7f7a539773fa5de24100b139f4f0188f8d93
DecryptLife <benson.thomas2602@gmail.com>
2024-06-05 13:10:17 -0500

    todolist-redux with ts

---
+++ src/redux/Slice/TodoSlice.tsx
@@ -0,0 +1,76 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { getTodos, createTodo, updateTodo, deleteTodo  } from "../../APIs/todoAPIs"
+
+
+interface ITodo{
+    id?: string,
+    content: string
+}
+
+type TodoState = ITodo[]
+
+const initialState:TodoState = []
+
+const fetchTodos = createAsyncThunk<TodoState>(
+    "todos/fetchTodos",
+    async () => {
+        const response = await getTodos();
+        console.log("In fetch todos: ", response)
+        return response as TodoState ;
+    }
+)
+
+const addTodo = createAsyncThunk<ITodo, ITodo>(
+    "todos/addTodo",
+    async(payload: ITodo): Promise<ITodo> => {
+        const response = await createTodo(payload);
+
+        console.log(response);
+        return response;
+    }
+)
+
+const editTodo = createAsyncThunk("todos/editTodo",async (payload:ITodo) => {
+
+    const response = await updateTodo(payload.id, payload.content);
+
+    console.log(response);
+
+    return response
+})
+
+const removeTodo = createAsyncThunk<string,string>("todos/removeTodo", async (id:string) => {
+    const response = await deleteTodo(id)
+
+    console.log("Remove todo: ", response);
+
+    return response;
+})
+
+export const todoSlice = createSlice({
+    name: "todos",
+    initialState,
+    reducers:{},
+    extraReducers:(builder) => {
+        builder.addCase(fetchTodos.fulfilled, (state, action:PayloadAction<TodoState>) => {
+            return action.payload;
+        })
+        .addCase(addTodo.fulfilled, (state, action:PayloadAction<ITodo>) => {
+            state.push(action.payload)
+        })
+        .addCase(removeTodo.fulfilled, (state, action: PayloadAction<string>) => {
+            return state.filter((todo) => todo.id !== action.payload)
+        })
+        .addCase(editTodo.fulfilled, (state, action) => {
+            const idx = state.findIndex((todo) => todo.id === action.payload.id)
+            if(idx !== -1){
+                state[idx] = action.payload
+            }
+        
+        } )
+    }
+})
+
+export {fetchTodos, addTodo, editTodo, removeTodo};
+
+export default todoSlice.reducer
